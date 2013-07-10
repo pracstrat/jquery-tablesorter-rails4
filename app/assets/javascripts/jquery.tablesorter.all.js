9e503d633ed9c7306ca6f09d7967afe4fcc83f2c
Jordan <jordan@51shepherd.com>
2013-07-10 08:41:24 +0800

    load all js

--- app/assets/javascripts/jquery.tablesorter.all.js
+++ app/assets/javascripts/jquery.tablesorter.all.js
@@ -1,7 +1,7 @@
 //
 //= require jquery_tablesorter/jquery.tablesorter
 //= require jquery_tablesorter/jquery.metadata
-//= require jquery_tablesorter/jquery.widgets
+//= require jquery_tablesorter/jquery.tablesorter.widgets
 //= require jquery_tablesorter/jquery.tablesorter.widgets-filter-formatter
 //= require_tree jquery_tablesorter/widgets
 //= require_tree jquery_tablesorter/parsers
